741d699d1234d5c88a8f59abc670b58b3b638384
Server <musta700@gmail.com>
2021-06-23 08:55:35 +0300

    project2burgermenu

--- layout/project2/main.js
+++ layout/project2/main.js
@@ -5,3 +5,10 @@ function changeBgColor(color){
     const sec = document.querySelector('.sec');
     sec.style.background = color;
 }
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigator = document.querySelector('.navigator');
+    toggleMenu.classList.toggle('active');
+    navigator.classList.toggle('active');
+
+}
