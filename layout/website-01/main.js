3f66eb5988bdd75250a9771b3d4ec43bb96b7afa
Server <musta700@gmail.com>
2021-07-05 17:22:17 +0300

    website 1 done

--- layout/website-01/main.js
+++ layout/website-01/main.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll',function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky",window.scrollY > 0);
 });
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigator = document.querySelector('.navigator');
+    navigator.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+}
